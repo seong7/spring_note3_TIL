e95a7fb1582114422e71d9cf5f72dc17ca85797b
Seongjin <seongjinkim1123@gmail.com>
2020-01-09 10:05:43 +0900

    20200109

---
+++ src/main/java/com/bit/springNote3/dto/NoteDto.java
@@ -0,0 +1,18 @@
+package com.bit.springNote3.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NoteDto {
+	// AllArgsConstructor : 모든 필드들을 parameter로 받아 한번에 set 해주는 생성자 생성
+	// NoArgsConstructor : parameter가 없는 생성자 생성 ( 해당 클래스의 객체 생성시 사용 )
+	
+	private int id;
+	private String writer;
+	private String content;
+
+}
